94c5fdf782b8d058be54dcc758d4e74336a7f4a2
AravindKumpatla <44069151+Aravind-Kumpatla-SAP@users.noreply.github.com>
2020-04-23 15:35:56 +0530

    Create debian.sh

---
+++ PackageInstallations/Java1.8_Ansible_MariaDB/debian.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+java -version
+rv= echo $?
+if [[ "$rv" -ne 0 ]]
+then
+   echo "Installing JAVA 1.8.0"
+   sudo apt-get install openjdk-8-jre -y
+   echo "============JAVA-VERSION=============="
+   java -version
+   update-alternatives --config  java 1
+   update-alternatives --config  javac 1
+   echo "Java installation is completed successfully"
+elif [[ "$rv" -eq 0 ]]
+then
+   echo "Java is already installed"
+   exit 0
+else
+   echo "Installation failed"
+fi
