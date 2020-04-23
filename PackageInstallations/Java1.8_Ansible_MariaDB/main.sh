99f8687f79dc6ec17794a2e1cee37dd36f0940c8
AravindKumpatla <44069151+Aravind-Kumpatla-SAP@users.noreply.github.com>
2020-04-23 15:42:31 +0530

    Update main.sh

--- PackageInstallations/Java1.8_Ansible_MariaDB/main.sh
+++ PackageInstallations/Java1.8_Ansible_MariaDB/main.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-if [[ `which yum` ]]; then
+FILE=redhat.sh
+FILE1=debian.sh
+if [[ -f $FILE && `which yum` ]]; then
    IS_RHEL=1
    echo "OS belongs to RHEL Family"
    echo "Running the installations."
    sh -x ./redhat.sh
-elif [[ `which apt` ]]; then
+elif [[ -f $FILE1 && `which apt` ]]; then
    IS_UBUNTU=1
    echo "OS belongs to DEBIAN Family"
    echo "Running the installations."
