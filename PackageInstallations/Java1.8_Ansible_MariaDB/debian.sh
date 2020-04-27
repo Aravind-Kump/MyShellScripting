cbace698a5ce4e91c16a548acc09b5640bbe47fd
AravindKumpatla <44069151+Aravind-Kumpatla-SAP@users.noreply.github.com>
2020-04-27 14:33:41 +0530

    Update debian.sh

--- PackageInstallations/Java1.8_Ansible_MariaDB/debian.sh
+++ PackageInstallations/Java1.8_Ansible_MariaDB/debian.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 java=/usr/bin/java
+ansible=/usr/bin/ansible
+mariadb=debian.yml
 if [  -f "$java"  ]
 then
    echo "Java already installed"
@@ -7,10 +9,39 @@ else
    echo "Installing JAVA 1.8.0"
    sudo add-apt-repository ppa:webupd8team/java
    sudo apt-get update
-   echo "============JAVA-VERSION=============="
    sudo apt-get install openjdk-8-jre-headless
    sudo apt-get install openjdk-8-jdk-headless
+   echo "============JAVA-VERSION=============="
    java -version
    echo "Java installation is completed successfully"
    exit 0
+fi
+if [  -f "$ansible"  ]
+then
+   echo "Ansible already exists"
+   ansible -version
+else
+   echo "Installing ansible"
+   sudo apt install software-properties-common
+   sudo apt-add-repository --yes --update ppa:ansible/ansible
+   sudo apt install ansible
+   ansible --version
+   exit 0
+fi
+echo "Installing Maria DB"
+if [  -f "$mariadb"  ]
+then
+   echo "File exists"
+   echo "localhost ansible_connection=local" > /etc/ansible/hosts
+   apt install python-pip -y
+   echo "testing connection"
+   ansible -m ping localhost |  head -1 | grep SUCCESS | awk '{print $3}'
+   if [  $? -eq 0  ]; then
+       echo " Proceeding for mariadb installation"
+       ansible-playbook ./debian.yml
+   else
+       echo "Please validate the ansible connections"
+   fi
+else
+   echo "File doesnot exist, cannot proceed for installations"
 fi
