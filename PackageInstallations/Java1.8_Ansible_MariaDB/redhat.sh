2432fafc27b3b87e520863eab6f7ed6dd9cd6a29
AravindKumpatla <44069151+Aravind-Kumpatla-SAP@users.noreply.github.com>
2020-04-27 16:17:31 +0530

    Update redhat.sh

--- PackageInstallations/Java1.8_Ansible_MariaDB/redhat.sh
+++ PackageInstallations/Java1.8_Ansible_MariaDB/redhat.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 java=/usr/bin/java
-if [[ -f "$java" ]]
+ansible=/usr/bin/ansible
+mariadb=centos.yml
+if [  -f "$java"  ]
 then
-   echo "Java is already installed"
-   exit 0
-
+   echo "Java already installed"
 else
    echo "Installing JAVA 1.8.0"
    sudo yum install java-1.8.0-openjdk-devel -y
@@ -13,4 +13,30 @@ else
    update-alternatives --config  java 1
    update-alternatives --config  javac 1
    echo "Java installation is completed successfully"
+fi
+if [  -f "$ansible"  ]
+then
+   echo "Ansible already exists"
+   ansible -version
+else
+   echo "Installing ansible"
+   yum install ansible -y
+   ansible --version
+fi
+echo "Installing Maria DB"
+if [  -f "$mariadb"  ]
+then
+   echo "File exists"
+   echo "localhost ansible_connection=local" > /etc/ansible/hosts
+   yum install python-pip -y
+   echo "testing connection"
+   ansible -m ping localhost |  head -1 | grep SUCCESS | awk '{print $3}'
+   if [  $? -eq 0  ]; then
+       echo " Proceeding for mariadb installation"
+       ansible-playbook ./centos.yml
+   else
+       echo "Please validate the ansible connections"
+   fi
+else
+   echo "File doesnot exist, cannot proceed for installations"
 fi
