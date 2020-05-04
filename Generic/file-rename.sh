52cedea873e1b25d0cd93ee67cad3a57bb9b1e81
AravindKumpatla <44069151+Aravind-Kumpatla-SAP@users.noreply.github.com>
2020-05-04 18:40:37 +0530

    Update file-rename.sh

--- Generic/file-rename.sh
+++ Generic/file-rename.sh
@@ -3,3 +3,14 @@ for file in *.txt
 do
   mv "$file" "${file/.txt/.log}"
 done
+
+
+SECOND FORMAT
+#!/bin/bash
+path="/tmp/folder/*.txt"
+for FILE in $path
+do
+   FILEBASENAME=$(echo $FILE | cut -d. -f1)
+   FILEEXT=$(echo $FILE | cut -d. -f2)
+   cp $FILE $FILEBASENAME-moved.html
+done
